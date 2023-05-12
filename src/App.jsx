14968261a765dfa0e2b5cd8098498719fa12ef0b
furaihan <28698437+furaihan@users.noreply.github.com>
2023-05-12 23:41:17 +0700

    component Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,11 @@
 import "./App.css";
-import logo from "./assets/image/logo.svg";
-import searchlogo from "./assets/image/search.svg";
+import Navbar from "./component/Navbar/Navbar";
 
 function App() {
   return (
     <>
       <main>
-        <nav className="navbar">
-          <div className="navbar-logo-menu">
-            <div className="navbar-brand">
-              <a className="navbar-item" href="#">
-                <img
-                  src={logo}
-                  className="logo"
-                  alt=""
-                  width="112"
-                  height="28"
-                />
-              </a>
-            </div>
-            <div className="navbar-menu">
-              <a className="navbar-item" href="#">
-                Home
-              </a>
-              <a className="navbar-item" href="#">
-                Explore
-              </a>
-              <a className="navbar-item" href="#">
-                About
-              </a>
-              <a className="navbar-item" href="#">
-                Support
-              </a>
-            </div>
-          </div>
-          <div className="navbar-search-login">
-            <div className="navbar-search">
-              <img src={searchlogo} alt="Search" className="search-logo" />
-            </div>
-          </div>
-        </nav>
+        <Navbar/>
       </main>
     </>
   );
---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import logo from "../../assets/image/logo.svg";
+import searchlogo from "../../assets/image/search.svg";
+import "./Navbar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo-menu">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="#">
+            <img src={logo} className="logo" alt="" width="112" height="28" />
+          </a>
+        </div>
+        <div className="navbar-menu">
+          <a className="navbar-item" href="#">
+            Home
+          </a>
+          <a className="navbar-item" href="/explore">
+            Explore
+          </a>
+          <a className="navbar-item" href="#">
+            About
+          </a>
+          <a className="navbar-item" href="#">
+            Support
+          </a>
+        </div>
+      </div>
+      <div className="navbar-search-login">
+        <div className="navbar-search">
+          <img src={searchlogo} alt="Search" className="search-logo" />
+        </div>
+        <div className="login-button"></div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
