eb21c7c29a8d52b1b3fbd480bc5da21d77ec6967
unikts <unik.t@students.amikom.ac.id>
2023-06-02 15:04:41 +0700

    hapus-hapus

--- src/routes/Explore/Explore.jsx
+++ src/routes/Explore/Explore.jsx
@@ -1,19 +1,12 @@
 import Navbar from "../../component/Navbar/Navbar";
-import SearchBar from "../../component/SearchBar/SearchBar";
+import "./Explore.css"
 
 function Explore() {
   return (
     <>
       <main>
         <Navbar />
-        <div>
-          <h1>Explore</h1>
-          <p>This is the explore page.</p>
-          <SearchBar />
-        </div>
-        <section id="hero">
-
-        </section>
+        
       </main>
     </>
   );
