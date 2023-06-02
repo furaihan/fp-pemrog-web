b677828d3d0c2e834c7b6d790f0b1469fe7a48e2
nabellaag <nabellaayug@students.amikom.ac.id>
2023-06-02 14:20:37 +0700

    explore atas

--- src/routes/Explore/Explore.jsx
+++ src/routes/Explore/Explore.jsx
@@ -1,19 +1,38 @@
 import Navbar from "../../component/Navbar/Navbar";
 import SearchBar from "../../component/SearchBar/SearchBar";
+import ArrowDown from "../../assets/image/arrow-down.jsx";
 
 function Explore() {
   return (
     <>
       <main>
         <Navbar />
-        <div>
-          <h1>Explore</h1>
-          <p>This is the explore page.</p>
-          <SearchBar />
-        </div>
-        <section id="hero">
-
+        <section className="hero">
+          <div className="hero-body">
+            <p className="hero-title">Lorem Ipsum</p>
+            <p className="hero-subtitle">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              nec iaculis mauris.
+            </p>
+            <div className="arrow-down">
+              <ArrowDown />
+            </div>
+          </div>
         </section>
+        <section className="filter-animal">
+          <div className="filter-animal-body">
+           <h5 className='filter-animal-type'> Type </h5>
+           <div>
+            
+           </div>
+           <h5 className="filter-animal-region"> Region</h5>
+           <h5 className="filter-animal-environtment"> Environtment</h5>
+          </div>
+          
+        </section>
+
+        
+
       </main>
     </>
   );
