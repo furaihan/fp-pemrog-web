731a55ae0bc4f5f5f0e106ef8051a843ca181202
unikts <unik.t@students.amikom.ac.id>
2023-06-10 10:11:38 +0700

    menambahkan footer

--- src/routes/Explore/Explore.jsx
+++ src/routes/Explore/Explore.jsx
@@ -4,6 +4,7 @@ import "./Explore.css";
 import Environtment from "../../component/Dropdown/Environtment";
 import Region from "../../component/Dropdown/Region";
 import Type from "../../component/Dropdown/Type";
+import Footer from "../../component/Footer/Footer";
 
 function Explore() {
   return (
@@ -40,22 +41,15 @@ function Explore() {
               <div className="container-2">
                 <div className="row-2">
                   <div className="button-filter">
-                    
                     <button className="filter-button-filter"> Filter </button>
                   </div>
                   <div className="button-reset">
-                      
-                      <button className="filter-button-reset"> Reset </button>
+                    <button className="filter-button-reset"> Reset </button>
                   </div>
                 </div>
               </div>
-
-              </div>
-              
-            
+            </div>
           </div>
-          
-         
         </section>
         <br />
         <section className="list-animal">
@@ -106,7 +100,7 @@ function Explore() {
             </div>  
           </div>
         </section>
-        
+        <Footer />
       </main>
     </>
   );
