1f7cd19d584485d124906b2386840ed95aa4e9a2
furaihan <28698437+furaihan@users.noreply.github.com>
2023-06-16 11:30:56 +0700

    migration modify users

---
+++ server/migrations/20230616042122-modify-column.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    queryInterface.changeColumn("users", "first_name", {
+      type: Sequelize.STRING,
+      allowNull: true,
+      defaultValue: null,
+      field: "first_name",
+    });
+    queryInterface.changeColumn("users", "last_name", {
+      type: Sequelize.STRING,
+      allowNull: true,
+      defaultValue: null,
+      field: "last_name",
+    });
+    queryInterface.sequelize.query(
+      "ALTER TABLE `users` CHANGE `username` `username` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL AFTER `account_id`;"
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    queryInterface.changeColumn("users", "first_name", {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: "first_name",
+    });
+    queryInterface.changeColumn("users", "last_name", {
+      type: Sequelize.STRING,
+      allowNull: false,
+      field: "last_name",
+    });
+    queryInterface.sequelize.query(
+      "ALTER TABLE `users` CHANGE `username` `username` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL AFTER `last_name`;"
+    );
+  },
+};
