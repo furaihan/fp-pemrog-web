f9b4ce96d359ce5cd23f434bece5047d1971c1e5
nabellaag <nabellaayug@students.amikom.ac.id>
2023-06-18 11:03:10 +0700

    Quiz page

--- src/routes/Quiz/Quiz.jsx
+++ src/routes/Quiz/Quiz.jsx
@@ -1,11 +1,10 @@
 import "./Quiz.css";
-import Navbar from "../../component/Navbar/Navbar";
 
 function Quiz(){
     return(
         <>
             <main>
-                <Navbar />
+                
             </main>
         </>
     );
