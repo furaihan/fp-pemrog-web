4379bef3168bfe0befe974e2b630e2f4680b077a
furaihan <28698437+furaihan@users.noreply.github.com>
2023-06-21 16:45:49 +0700

    oke oke

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
 import Root from "./routes/Root/Root.jsx";
 import ErrorPage from "./ErrorPage.jsx";
 import About from "./routes/About/About.jsx";
 import Explore from "./routes/Explore/Explore.jsx";
 import Train from "./routes/Train/Train.jsx";
 import Login from "./routes/Login/Login.jsx";
-import Signup, {registerAction} from "./routes/Signup/Signup.jsx";
+import Signup, { registerAction } from "./routes/Signup/Signup.jsx";
 import Description from "./routes/Explore/Description.jsx";
 import Quiz from "./routes/Quiz/Quiz.jsx";
 
@@ -25,12 +30,12 @@ const router = createBrowserRouter(
         <Route path="explore" element={<Explore />} />
         <Route path="description" element={<Description />} />
         <Route path="train" element={<Train />} />
-        <Route path="quiz" element={<Quiz />} />
       </Route>
       <Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} action={registerAction} />
       </Route>
+      <Route path="/quiz" element={<Quiz />} />
       <Route path="*" element={<ErrorPage />} />
     </Route>
   )
