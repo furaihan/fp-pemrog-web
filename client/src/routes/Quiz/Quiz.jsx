1948f89ebf8a0fec40a97704674dd0db26a9837e
nabellaag <nabellaayug@students.amikom.ac.id>
2023-06-21 21:25:07 +0700

    penyeseuaian warna pada page quiz

--- client/src/routes/Quiz/Quiz.jsx
+++ client/src/routes/Quiz/Quiz.jsx
@@ -4,14 +4,18 @@ import "./Quiz.css";
 function Quiz() {
   return (
     <div className="quiz">
-      <div className="quiz-jumlah-soal">
-        <h1>1/5</h1>
-      </div>
-      <div className="top">
-        <div className="timer"> 30 </div>
-      </div>
-      <div className="bottom">
-        <QnA />
+      <div className="quiz border outside">
+        <div className="quiz border inside">
+          <div className="quiz-jumlah-soal">
+            <h2>1/5</h2>
+          </div>
+          <div className="top">
+            <div className="timer"> 30 </div>
+          </div>
+          <div className="bottom">
+            <QnA />
+          </div>
+        </div>
       </div>
     </div>
   );
