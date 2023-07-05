411311d4f140fd83ea64b7b5ead7123b0fa3e237
nabellaag <nabellaayug@students.amikom.ac.id>
2023-07-05 21:14:15 +0700

    hamburger

--- client/src/component/Hamburger/Hamburger.jsx
+++ client/src/component/Hamburger/Hamburger.jsx
@@ -1,26 +1,30 @@
-import "./Hamburger.css";
+import React, { useState } from 'react';
+import './Hamburger.css';
 
 function Hamburger() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar2">
       <div className="navbar2-container container">
-          <input type="checkbox" name="" id=""/>
-          <div className="hamburger-lines">
-              <span className="line line1"></span>
-              <span className="line line2"></span>
-              <span className="line line3"></span>
-          </div>
-          <ul className="menu-items">
-              <li><a href="#home">Home</a></li>
-              <li><a href="#about">Explore</a></li>
-              <li><a href="#food">About</a></li>
-              <li><a href="#food-menu">Support</a></li>
-          </ul>
-          <div className="logo">
-            
-          </div>
+        <input type="checkbox" name="" id="" checked={isOpen} onChange={toggleMenu} />
+        <div className="hamburger-lines">
+          <span className="line line1"></span>
+          <span className="line line2"></span>
+          <span className="line line3"></span>
+        </div>
+        <ul className={`menu-items ${isOpen ? 'open' : ''}`}>
+          <li><a href="/">Home</a></li>
+          <li><a href="/explore">Explore</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/support">Support</a></li>
+        </ul>
+        <div className="logo"></div>
       </div>
-  </nav>
+    </nav>
   );
 }
 
