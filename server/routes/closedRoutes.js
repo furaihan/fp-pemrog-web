f8bd17cd464e271cef390002f2f1885909b1be12
furaihan <28698437+furaihan@users.noreply.github.com>
2023-07-05 21:04:33 +0700

    profile router

--- server/routes/closedRoutes.js
+++ server/routes/closedRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const getUser = require("../controller/getUser");
+const getProfile = require("../controller/getProfile");
 
 const router = express.Router();
 
 router.get("/user/current", getUser);
+router.get("/user/profile", getProfile);
 
 module.exports = router;
