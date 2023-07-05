5409ab4b56add5e161f647e8b6c85532cf6e3f10
furaihan <28698437+furaihan@users.noreply.github.com>
2023-07-05 23:20:41 +0700

    react-query

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -13,8 +13,10 @@ module.exports = {
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@tanstack/query'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "@tanstack/query/exhaustive-deps": "error",
+    "@tanstack/query/prefer-query-object-syntax": "error"
   },
 }
