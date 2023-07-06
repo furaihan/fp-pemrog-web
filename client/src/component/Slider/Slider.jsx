eb54454eb0f5b85944a86330f146ebb186e654ca
furaihan <28698437+furaihan@users.noreply.github.com>
2023-07-06 11:48:37 +0700

    some fixes

--- client/src/component/Slider/Slider.jsx
+++ client/src/component/Slider/Slider.jsx
@@ -1,25 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import './Slider.css';
-import { HiChevronDoubleDown } from 'react-icons/hi';
-
+import { useState, useEffect } from "react";
+import "./Slider.css";
+import { HiChevronDoubleDown } from "react-icons/hi";
 
 function Slider() {
   const images = [
     {
-      src: 'homie.jpg',
-      alt: 'Image 1',
-      caption: 'Hai Brooo'
+      src: "homie.jpg",
+      alt: "Image 1",
+      caption: "Hai Brooo",
     },
     {
-      src: 'irish-elk.jpg',
-      alt: 'Image 2',
-      caption: 'Welcome'
+      src: "irish-elk.jpg",
+      alt: "Image 2",
+      caption: "Welcome",
     },
     {
-      src: 'takahe.jpg',
-      alt: 'Image 3',
-      caption: 'to My Jungle'
-    }
+      src: "takahe.jpg",
+      alt: "Image 3",
+      caption: "to My Jungle",
+    },
   ];
 
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -35,36 +34,40 @@ function Slider() {
   }, [images.length]);
 
   const goToPreviousImage = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
   };
 
   const goToNextImage = () => {
     setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
   };
   const scrollToBottom = () => {
-    window.scrollTo({ top: document.documentElement.scrollHeight, behavior: 'smooth' });
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
   };
   return (
     <div className="slider">
       {images.map((image, index) => (
         <div
           key={index}
-          className={index === currentImageIndex ? 'slide active' : 'slide'}
+          className={index === currentImageIndex ? "slide active" : "slide"}
         >
-          <img
-            src={image.src}
-            alt={image.alt}
-            className="slide-image"
-          />
+          <img src={image.src} alt={image.alt} className="slide-image" />
           <div className="caption">{image.caption}</div>
         </div>
-       ))}
+      ))}
       <div className="arrow-down" onClick={scrollToBottom}>
         <HiChevronDoubleDown />
       </div>
-      <button className="previous-button" onClick={goToPreviousImage}><img className="previous-img"
-      src="chevron-left.svg" alt="" /></button>
-      <button className="next-button" onClick={goToNextImage}><img className="next-img" src="chevron-right.svg" alt="" /></button>
+      <button className="previous-button" onClick={goToPreviousImage}>
+        <img className="previous-img" src="chevron-left.svg" alt="" />
+      </button>
+      <button className="next-button" onClick={goToNextImage}>
+        <img className="next-img" src="chevron-right.svg" alt="" />
+      </button>
     </div>
   );
 }
