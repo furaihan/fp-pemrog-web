2972282b59d32edaf71b4e4e3802382987f8f2e8
furaihan <28698437+furaihan@users.noreply.github.com>
2023-07-06 13:53:56 +0700

    model redefinition after migration

--- server/models/description.js
+++ server/models/description.js
@@ -1,56 +1,56 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-    class Description extends Model {
-        static associate(models) {
-            this.Animal = this.belongsTo(models.Animal, {
-                foreignKey: {
-                    name: "animal_id",
-                    allowNull: false,
-                    type: DataTypes.INTEGER,
-                },
-            });
-        }
-    }
-    Description.init(
-        {
-            description_id: {
-                type: DataTypes.INTEGER,
-                autoIncrement: true,
-                primaryKey: true,
-                allowNull: false,
-                unique: true,
-                field: "description_id",
-            },
-            title: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                field: 'title',
-              },
-              description: {
-                type: DataTypes.TEXT,
-                allowNull: false,
-                field: 'description',
-              },
-              image: {
-                type: DataTypes.STRING,
-                allowNull: false,
-                field: 'image',
-              },
-              fun_fact: {
-                type: DataTypes.TEXT,
-                allowNull: false,
-                field: 'fun_fact',
-              },
+  class Description extends Model {
+    static associate(models) {
+      this.Animal = this.belongsTo(models.Animal, {
+        foreignKey: {
+          name: "animal_id",
+          allowNull: false,
+          type: DataTypes.INTEGER,
         },
-        {
-            sequelize,
-            modelName: "Description",
-            tableName: "descriptions",
-            freezeTableName: true,
-            timestamps: false,
-            underscored: true,
-        }
-    );
-    return Description;
+      });
+    }
+  }
+  Description.init(
+    {
+      description_id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+        field: "description_id",
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "title",
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        field: "description",
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        field: "image",
+      },
+      fun_fact: {
+        type: DataTypes.TEXT,
+        allowNull: true,
+        field: "fun_fact",
+      },
+    },
+    {
+      sequelize,
+      modelName: "Description",
+      tableName: "descriptions",
+      freezeTableName: true,
+      timestamps: false,
+      underscored: true,
+    }
+  );
+  return Description;
 };
