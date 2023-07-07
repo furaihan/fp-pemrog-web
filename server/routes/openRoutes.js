a038276e686c0b470e23c726cf67ad424c02c4b8
furaihan <28698437+furaihan@users.noreply.github.com>
2023-07-07 10:42:45 +0700

    animal route

--- server/routes/openRoutes.js
+++ server/routes/openRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { signup, login } = require("../controller");
+const getAnimals = require("../controller/animals");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/animals", getAnimals);
 
 module.exports = router;
